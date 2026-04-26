da80f238a609c286dddae62e514c2915ce9d9a81
Santi GM <gimeno.profe@gmail.com>
2026-04-26 14:52:52 +0200

    The game is the same than 7 years ago, but now is refactor to run in only one process, and not open one again every time you move over directories

---
+++ README.md
@@ -0,0 +1,33 @@
+# rpgBashGame
+Linux and OSX game to learn Linux / Unix console commands
+
+Install
+------------
+
+Download all files, install it with:
+
+sudo ./install.sh
+
+or 
+
+bash install.sh
+
+
+Game will be installed in HOME/rpgBashGame. 
+
+
+How to play a new game from beginning:
+--------
+
+Just run: 
+
+masria
+
+
+Other things:
+--------
+
+Remember, to move in the game you can use "move" or "cd" command.
+
+Have a nice time.
+
---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+
+#sudo apt update
+#sudo apt install feh
+
+# move es un 'cd vitaminado. Lo que hace es un cd,
+# luego limpia la pantalla, y muestra alguna imagen o .escenario de la pantalla a la que llegas.
+
+gamedir='$HOME/MasriaBashGame'
+
+# Si ya existe el juego, lo borra.
+if [[ -d $gamedir ]]
+then
+	rm -rf $gamedir
+fi
+
+# aqui un if (si ya habia partida anterior, preguntar si quiere nueva o seguir.)
+
+# Pasos de la instalacion. carpeta y ejecutable
+mkdir -p $HOME/MasriaBashGame && \
+mkdir -p $HOME/MasriaBashGame/bin && \
+
+#cp move $HOME/bin/move
+#chmod +x $HOME/bin/move
+#cp cat_game $HOME/bin/cat_game
+#chmod +x $HOME/bin/cat_game
+#cp continuegame $HOME/bin/continuegame
+#chmod +x $HOME/bin/continuegame
+
+cp Masria.tar.gz $HOME/MasriaBashGame/Masria.tar.gz && \
+cp caratula.png $HOME/MasriaBashGame/caratula.png && \
+
+cp masria $HOME/MasriaBashGame/bin/masria && \
+chmod +x $HOME/MasriaBashGame/bin/masria && \
+
+#preparar bashrc_game
+cp bashrc_game $HOME/MasriaBashGame/bashrc_game && \
+
+
+# Descomprimir escenario Limpio nueva partida:
+cd $HOME/MasriaBashGame && \
+tar -xzvf $HOME/MasriaBashGame/Masria.tar.gz && \
+
+
+
+#MAC OS X
+PATH=$PATH:$HOME/MasriaBashGame/bin && \
+
+echo "PATH=$PATH:$HOME/MasriaBashGame/bin" >> $HOME/.bashrc && \
+echo "PATH=$PATH:$HOME/MasriaBashGame/bin" >> $HOME/.zshrc
+
+#echo "export BASH_SILENCE_DEPRECATION_WARNING=1" >> $HOME/MasriaBashGame/bashrc_game
+
+if [[ $? -eq 0 ]]
+then
+	echo "El juego se ha instalado correctamente."
+	read -p "Pulsa [INTRO] para empezar una nueva partida." aaa
+	masria
+	
+else
+	echo "algo no ha ido bien en la instalacion."
+fi
